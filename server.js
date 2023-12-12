e025a6711d8f633c333680822e125646efba7a11
nmturr <nagymiklosimatyas@turr.hu>
2023-12-12 12:51:36 +0100

    Adat felvétel kész

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+var mysql = require('mysql');
+require('dotenv').config();
+const cors = require('cors');
+const { handle } = require('express/lib/application');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json());
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME,
+    timezone: 'UTC'
+});
+
+app.post("/dolgozat", (req, res) => {
+    let table = "dolgozatok";
+    let body = req.body;
+    pool.query(`INSERT INTO dolgozatok VALUES (null, '${body.nev}', '${body.dolgozatnev}', '${body.leiras}', '${body.datum}', '${body.konzulensnev}', ${body.ertekeles})`, (err, results) => {
+        handleError(err);
+        sendResults(req, res, results, 1, table, "record inserted into");
+    });
+});
+
+app.get("/dolgozat", (req, res) => {
+    
+});
+
+app.get("/dolgozat/:id", (req, res) => {
+    
+});
+
+app.patch("/dolgozat/:id", (req, res) => {
+    
+});
+
+app.delete("/dolgozat/:id", (req, res) => {
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
+function sendResults(req, res, results, affectedRows, table, message){
+    console.log(`${req.socket.remoteAddress}: ${affectedRows} ${message} ${table}.`);
+    res.send(results);
+}
+
+function handleError(err){
+    if (err) {
+        console.log(err.sqlMessage)
+    }
+}
